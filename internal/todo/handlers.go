b1ce20a991bc9f9e0f768c785a2a335db6c1cecd
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-26 19:17:24 +0545

    updated the handler logic.

--- internal/todo/handlers.go
+++ internal/todo/handlers.go
@@ -3,32 +3,68 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/google/uuid"
 	"github.com/userAdityaa/todo-backend/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+func GetAllTodo(collection *mongo.Collection) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var todos []models.Todo
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		cursor, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer cursor.Close(ctx)
+
+		for cursor.Next(ctx) {
+			var todo models.Todo
+			if err := cursor.Decode(&todo); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			todos = append(todos, todo)
+		}
+
+		if err := cursor.Err(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(todos)
+	}
+}
 
 func CreateTodo(collection *mongo.Collection) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var todo models.Todo
-		log.Println(json.NewDecoder(r.Body))
 		if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
 			log.Println(err)
 			http.Error(w, "Invalid request payload", http.StatusNotAcceptable)
 			return
 		}
 
+		todo.ID = primitive.NewObjectID()
+
 		if todo.Name == "" {
 			http.Error(w, "Name and DueDate are required field", http.StatusBadRequest)
 			return
 		}
 
-		result, err := collection.InsertOne(context.TODO(), todo)
+		_, err := collection.InsertOne(context.TODO(), todo)
 		if err != nil {
 			log.Fatal(err)
 			return
@@ -37,8 +73,8 @@ func CreateTodo(collection *mongo.Collection) http.HandlerFunc {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
 		response := map[string]interface{}{
-			"message":     "Todo Created Successfully",
-			"inserted_id": result.InsertedID,
+			"message": "Todo Created Successfully",
+			"id":      todo.ID.Hex(),
 		}
 
 		json.NewEncoder(w).Encode(response)
@@ -76,16 +112,15 @@ func UpdateTodo(collection *mongo.Collection) http.HandlerFunc {
 			return
 		}
 
-		var updatedTodo models.Todo
-
-		if err := json.NewDecoder(r.Body).Decode(&updatedTodo); err != nil {
-			http.Error(w, err.Error(), http.StatusBadGateway)
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read request body", http.StatusInternalServerError)
 			return
 		}
+		var updatedTodo models.Todo
 
-		todoId, err := uuid.Parse(id)
-		if err != nil {
-			http.Error(w, "Invalid UUID", http.StatusBadRequest)
+		if err := json.Unmarshal(body, &updatedTodo); err != nil {
+			http.Error(w, "Invalid JSON format: "+err.Error(), http.StatusBadRequest)
 			return
 		}
 
@@ -99,16 +134,19 @@ func UpdateTodo(collection *mongo.Collection) http.HandlerFunc {
 			},
 		}
 
-		filter := bson.M{"_id": todoId}
-
-		result, err := collection.UpdateOne(context.TODO(), filter, update)
+		filterID, err := primitive.ObjectIDFromHex(id)
 		if err != nil {
-			http.Error(w, "Failed to update todo", http.StatusInternalServerError)
+			http.Error(w, "Invalid ObjectID format", http.StatusBadRequest)
 			return
 		}
 
-		if result.MatchedCount == 0 {
-			http.Error(w, "Todo not found", http.StatusNotFound)
+		filter := bson.M{"_id": filterID}
+
+		result, err := collection.UpdateOne(context.TODO(), filter, update)
+
+		fmt.Println(err)
+		if err != nil {
+			http.Error(w, "Failed to update todo", http.StatusInternalServerError)
 			return
 		}
 
