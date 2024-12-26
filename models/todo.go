bdc730bec9f2cabd2d92d2d54df0975f65098818
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-26 19:16:35 +0545

    todo_model: change the datatype of model

--- models/todo.go
+++ models/todo.go
@@ -1,14 +1,14 @@
 package models
 
 import (
-	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Todo struct {
-	ID          uuid.UUID `json:"id" bson:"_id"`
-	Name        string    `json:"name" bson:"name"`
-	Description string    `json:"description" bson:"description"`
-	List        []string  `json:"list" bson:"list"`
-	DueDate     string    `json:"due_date" bson:"due_date"`
-	Subtask     []string  `json:"sub_task" bson:"sub_task"`
+	ID          primitive.ObjectID `json:"id" bson:"_id"`
+	Name        string             `json:"name" bson:"name"`
+	Description string             `json:"description" bson:"description"`
+	List        string             `json:"list" bson:"list"`
+	DueDate     string             `json:"due_date" bson:"due_date"`
+	Subtask     []string           `json:"sub_task" bson:"sub_task"`
 }
