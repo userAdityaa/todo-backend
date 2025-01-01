2d912750381ffbc1953b1429abd5172839577891
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-01 14:18:03 +0545

    todo_routes: setup the todo routes.

--- routes/todo_routes.go
+++ routes/todo_routes.go
@@ -8,7 +8,7 @@ import (
 
 func SetUpTodoRoutes(router *chi.Mux, collection *mongo.Collection) {
 	router.Post("/create-todo", handlers.CreateTodo(collection))
-	router.Delete("/delete-todo", handlers.DeleteTodo(collection))
+	router.Delete("/delete-todo/{id}", handlers.DeleteTodo(collection))
 	router.Put("/update-todo/{id}", handlers.UpdateTodo(collection))
 	router.Get("/all-todo", handlers.GetAllTodo(collection))
 }
