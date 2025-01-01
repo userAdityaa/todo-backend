dce9a81c7d632d27c29c9bdb3e043ec5dd4b21c2
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-01 18:51:55 +0545

    validate_jwt: Implemented the logic for validating jwt.

---
+++ utils/jwt_validate.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"errors"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var secretKey = []byte("your_secret_key")
+
+func ValidateToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("Unexpected signing method.")
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("Invalid Token")
+}
