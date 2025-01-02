78302713c14f5709b575b0ada2844c85fbe71e7f
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-02 18:44:27 +0545

    model_schema: updated the model schema for including user with their todo

--- models/model.go
+++ models/model.go
@@ -12,3 +12,11 @@ type Todo struct {
 	DueDate     string             `json:"due_date" bson:"due_date"`
 	Subtask     []string           `json:"sub_task" bson:"sub_task"`
 }
+
+type User struct {
+	ID      string `json:"id" bson:"_id"`
+	Name    string `json:"name" bson:"username"`
+	Email   string `json:"email" bson:"email"`
+	Picture string `json:"picture" bson:"picture"`
+	Todo    []Todo `json:"todos" bson:"todos"`
+}
--- models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	ID      string `json:"id" bson:"_id"`
-	Name    string `json:"name" bson:"username"`
-	Email   string `json:"email" bson:"email"`
-	Picture string `json:"picture" bson:"picture"`
-}
