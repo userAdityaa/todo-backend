62780c693718f8cd46485d0c72058c85a4fb4e4b
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-03 19:28:22 +0545

    sticky_routes: added the stick routes

---
+++ routes/stick_routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/go-chi/chi/v5"
+	handlers "github.com/userAdityaa/todo-backend/internal/sticky"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SetUpStickyRoutes(router *chi.Mux, stickCollection *mongo.Collection, userCollection *mongo.Collection) {
+	router.Post("/create-sticky", handlers.CreateSticky(stickCollection, userCollection))
+}
