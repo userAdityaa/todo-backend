93f3ba081b6819c2c9020a39bffaac322991537d
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-04 23:12:03 +0545

    list routes created

--- routes/list_routes.go
+++ routes/list_routes.go
@@ -2,9 +2,12 @@ package routes
 
 import (
 	"github.com/go-chi/chi/v5"
+	handlers "github.com/userAdityaa/todo-backend/internal/List"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func SetUpListRoutes(router *chi.Mux, listCollection *mongo.Collection, userCollection *mongo.Collection) {
-	// router.Post("/create-list", handlers.)
+	router.Post("/create-list", handlers.CreateList(listCollection, userCollection))
+	router.Delete("/delete-list", handlers.DeleteList(listCollection, userCollection))
+	router.Get("/all-list", handlers.GetAllList(listCollection, userCollection))
 }
