e652e4e57871bd334deb48f5e8a06c070a27ca4f
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-07 13:57:47 +0545

    configurd backend for serverless

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -12,17 +10,14 @@ import (
 	"github.com/userAdityaa/todo-backend/routes"
 )
 
-func main() {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	config.LoadConfig()
 	auth.InitGoogleOAuth(config.GoogleClientID, config.GoogleClientSecret, config.GoogleRedirectURL)
 	database := config.SetUpDataBase()
-	todoCollection := config.TodoCollection(database)
-	userCollection := config.UserCollection(database)
-	stickyCollection := config.StickyCollection(database)
-	listCollection := config.ListCollection(database)
-	eventCollection := config.EventCollection(database)
+
 	router := chi.NewMux()
 
+	// CORS Configuration
 	corsHandler := cors.New(cors.Options{
 		AllowedOrigins:   []string{"https://minimal-planner.vercel.app"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -31,20 +26,17 @@ func main() {
 		AllowCredentials: true,
 		MaxAge:           300,
 	})
-
 	router.Use(corsHandler.Handler)
-	routes.SetUpTodoRoutes(router, todoCollection, userCollection)
-	routes.SetUpStickyRoutes(router, stickyCollection, userCollection)
-	routes.SetUpListRoutes(router, listCollection, userCollection)
-	routes.SetUpEventRoutes(router, eventCollection, userCollection)
+
+	// Routes
+	routes.SetUpTodoRoutes(router, config.TodoCollection(database), config.UserCollection(database))
+	routes.SetUpStickyRoutes(router, config.StickyCollection(database), config.UserCollection(database))
+	routes.SetUpListRoutes(router, config.ListCollection(database), config.UserCollection(database))
+	routes.SetUpEventRoutes(router, config.EventCollection(database), config.UserCollection(database))
 	router.HandleFunc("/auth/google/login", auth.GoogleLoginHandler)
 	router.HandleFunc("/auth/google/callback", auth.GoogleCallBackHandler(database))
 	router.Get("/auth/user", auth.GetUserDetailsHandler(database))
-	port := config.LoadPort()
 
-	fmt.Printf("Connected Locally to port number: %s\n", port)
-	if err := http.ListenAndServe(port, router); err != nil {
-		log.Fatal(err)
-		return
-	}
+	// Serve the request
+	router.ServeHTTP(w, r)
 }
