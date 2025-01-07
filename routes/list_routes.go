6991fd2bf8b9c382f0d2b20033f8aa680f00fa4e
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-01-07 13:15:05 +0545

    added the single list getter using id

--- routes/list_routes.go
+++ routes/list_routes.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"github.com/go-chi/chi/v5"
-	handlers "github.com/userAdityaa/todo-backend/internal/List"
+	handlers "github.com/userAdityaa/todo-backend/internal/list"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -10,4 +10,5 @@ func SetUpListRoutes(router *chi.Mux, listCollection *mongo.Collection, userColl
 	router.Post("/create-list", handlers.CreateList(listCollection, userCollection))
 	router.Delete("/delete-list", handlers.DeleteList(listCollection, userCollection))
 	router.Get("/all-list", handlers.GetAllList(listCollection, userCollection))
+	router.Get("/lists/{id}", handlers.FindAList(listCollection, userCollection))
 }
